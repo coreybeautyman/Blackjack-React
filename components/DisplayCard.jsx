2ded4232f25dec92bdd49240ea2e3a79f415daf8
coreybeautyman <corey.beautyman@gmail.com>
2024-09-02 12:23:04 +0100

    fixing card images

--- components/DisplayCard.jsx
+++ components/DisplayCard.jsx
@@ -1,16 +1,12 @@
+import cardImages from './cardImages';
+
 function DisplayCard({ card, player }) {
   const playerClass = `card player--card player01-card-1 player01-card`;
   const dealerClass = `card dealer--card`;
 
   const classlist = player === 'player' ? playerClass : dealerClass;
 
-  return (
-    <img
-      src={`http://127.0.0.1:5500/cards/${card}.png`}
-      alt='card'
-      className={classlist}
-    />
-  );
+  return <img src={cardImages[card]} alt='card' className={classlist} />;
 }
 
 export default DisplayCard;
---
+++ components/cardImages.js
@@ -0,0 +1,124 @@
+import card_0_1 from '../src/assets/cards/0_1.png'; // Back of the card
+
+import card_1_1 from '../src/assets/cards/1_1.png'; // Ace of Clubs
+import card_1_2 from '../src/assets/cards/1_2.png'; // Ace of Diamonds
+import card_1_3 from '../src/assets/cards/1_3.png'; // Ace of Hearts
+import card_1_4 from '../src/assets/cards/1_4.png'; // Ace of Spades
+
+import card_2_1 from '../src/assets/cards/2_1.png'; // 2 of Clubs
+import card_2_2 from '../src/assets/cards/2_2.png'; // 2 of Diamonds
+import card_2_3 from '../src/assets/cards/2_3.png'; // 2 of Hearts
+import card_2_4 from '../src/assets/cards/2_4.png'; // 2 of Spades
+
+import card_3_1 from '../src/assets/cards/3_1.png'; // 3 of Clubs
+import card_3_2 from '../src/assets/cards/3_2.png'; // 3 of Diamonds
+import card_3_3 from '../src/assets/cards/3_3.png'; // 3 of Hearts
+import card_3_4 from '../src/assets/cards/3_4.png'; // 3 of Spades
+
+import card_4_1 from '../src/assets/cards/4_1.png'; // 4 of Clubs
+import card_4_2 from '../src/assets/cards/4_2.png'; // 4 of Diamonds
+import card_4_3 from '../src/assets/cards/4_3.png'; // 4 of Hearts
+import card_4_4 from '../src/assets/cards/4_4.png'; // 4 of Spades
+
+import card_5_1 from '../src/assets/cards/5_1.png'; // 5 of Clubs
+import card_5_2 from '../src/assets/cards/5_2.png'; // 5 of Diamonds
+import card_5_3 from '../src/assets/cards/5_3.png'; // 5 of Hearts
+import card_5_4 from '../src/assets/cards/5_4.png'; // 5 of Spades
+
+import card_6_1 from '../src/assets/cards/6_1.png'; // 6 of Clubs
+import card_6_2 from '../src/assets/cards/6_2.png'; // 6 of Diamonds
+import card_6_3 from '../src/assets/cards/6_3.png'; // 6 of Hearts
+import card_6_4 from '../src/assets/cards/6_4.png'; // 6 of Spades
+
+import card_7_1 from '../src/assets/cards/7_1.png'; // 7 of Clubs
+import card_7_2 from '../src/assets/cards/7_2.png'; // 7 of Diamonds
+import card_7_3 from '../src/assets/cards/7_3.png'; // 7 of Hearts
+import card_7_4 from '../src/assets/cards/7_4.png'; // 7 of Spades
+
+import card_8_1 from '../src/assets/cards/8_1.png'; // 8 of Clubs
+import card_8_2 from '../src/assets/cards/8_2.png'; // 8 of Diamonds
+import card_8_3 from '../src/assets/cards/8_3.png'; // 8 of Hearts
+import card_8_4 from '../src/assets/cards/8_4.png'; // 8 of Spades
+
+import card_9_1 from '../src/assets/cards/9_1.png'; // 9 of Clubs
+import card_9_2 from '../src/assets/cards/9_2.png'; // 9 of Diamonds
+import card_9_3 from '../src/assets/cards/9_3.png'; // 9 of Hearts
+import card_9_4 from '../src/assets/cards/9_4.png'; // 9 of Spades
+
+import card_10_1 from '../src/assets/cards/10_1.png'; // 10 of Clubs
+import card_10_2 from '../src/assets/cards/10_2.png'; // 10 of Diamonds
+import card_10_3 from '../src/assets/cards/10_3.png'; // 10 of Hearts
+import card_10_4 from '../src/assets/cards/10_4.png'; // 10 of Spades
+
+import card_11_1 from '../src/assets/cards/11_1.png'; // Jack of Clubs
+import card_11_2 from '../src/assets/cards/11_2.png'; // Jack of Diamonds
+import card_11_3 from '../src/assets/cards/11_3.png'; // Jack of Hearts
+import card_11_4 from '../src/assets/cards/11_4.png'; // Jack of Spades
+
+import card_12_1 from '../src/assets/cards/12_1.png'; // Queen of Clubs
+import card_12_2 from '../src/assets/cards/12_2.png'; // Queen of Diamonds
+import card_12_3 from '../src/assets/cards/12_3.png'; // Queen of Hearts
+import card_12_4 from '../src/assets/cards/12_4.png'; // Queen of Spades
+
+import card_13_1 from '../src/assets/cards/13_1.png'; // King of Clubs
+import card_13_2 from '../src/assets/cards/13_2.png'; // King of Diamonds
+import card_13_3 from '../src/assets/cards/13_3.png'; // King of Hearts
+import card_13_4 from '../src/assets/cards/13_4.png'; // King of Spades
+
+const cardImages = {
+  '0_1': card_0_1, // Back of the card
+  '1_1': card_1_1, // Ace of Clubs
+  '1_2': card_1_2, // Ace of Diamonds
+  '1_3': card_1_3, // Ace of Hearts
+  '1_4': card_1_4, // Ace of Spades
+  '2_1': card_2_1, // 2 of Clubs
+  '2_2': card_2_2, // 2 of Diamonds
+  '2_3': card_2_3, // 2 of Hearts
+  '2_4': card_2_4, // 2 of Spades
+  '3_1': card_3_1, // 3 of Clubs
+  '3_2': card_3_2, // 3 of Diamonds
+  '3_3': card_3_3, // 3 of Hearts
+  '3_4': card_3_4, // 3 of Spades
+  '4_1': card_4_1, // 4 of Clubs
+  '4_2': card_4_2, // 4 of Diamonds
+  '4_3': card_4_3, // 4 of Hearts
+  '4_4': card_4_4, // 4 of Spades
+  '5_1': card_5_1, // 5 of Clubs
+  '5_2': card_5_2, // 5 of Diamonds
+  '5_3': card_5_3, // 5 of Hearts
+  '5_4': card_5_4, // 5 of Spades
+  '6_1': card_6_1, // 6 of Clubs
+  '6_2': card_6_2, // 6 of Diamonds
+  '6_3': card_6_3, // 6 of Hearts
+  '6_4': card_6_4, // 6 of Spades
+  '7_1': card_7_1, // 7 of Clubs
+  '7_2': card_7_2, // 7 of Diamonds
+  '7_3': card_7_3, // 7 of Hearts
+  '7_4': card_7_4, // 7 of Spades
+  '8_1': card_8_1, // 8 of Clubs
+  '8_2': card_8_2, // 8 of Diamonds
+  '8_3': card_8_3, // 8 of Hearts
+  '8_4': card_8_4, // 8 of Spades
+  '9_1': card_9_1, // 9 of Clubs
+  '9_2': card_9_2, // 9 of Diamonds
+  '9_3': card_9_3, // 9 of Hearts
+  '9_4': card_9_4, // 9 of Spades
+  '10_1': card_10_1, // 10 of Clubs
+  '10_2': card_10_2, // 10 of Diamonds
+  '10_3': card_10_3, // 10 of Hearts
+  '10_4': card_10_4, // 10 of Spades
+  '11_1': card_11_1, // Jack of Clubs
+  '11_2': card_11_2, // Jack of Diamonds
+  '11_3': card_11_3, // Jack of Hearts
+  '11_4': card_11_4, // Jack of Spades
+  '12_1': card_12_1, // Queen of Clubs
+  '12_2': card_12_2, // Queen of Diamonds
+  '12_3': card_12_3, // Queen of Hearts
+  '12_4': card_12_4, // Queen of Spades
+  '13_1': card_13_1, // King of Clubs
+  '13_2': card_13_2, // King of Diamonds
+  '13_3': card_13_3, // King of Hearts
+  '13_4': card_13_4 // King of Spades
+};
+
+export default cardImages;
